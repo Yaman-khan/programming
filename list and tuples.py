22668b907bc5b0a47ce0ca2f3043a9479915f2a6
Yaman-khan <muhammadyaman10921092@gmail.com>
2024-09-27 20:58:48 +0500

    Add files via upload

---
+++ list and tuples.py
@@ -0,0 +1,53 @@
+# mark = [4,4,45,5,56,7,7]
+# print(type(mark))
+# print(mark[4]) 
+# # this line is used to find any number in the list on which rank is present line 3
+# print(len(mark)) 
+#use to find len of list
+# in list ue use different types of element
+# detail = ["yaman", "age", 20, "swabi"]
+# print(detail)
+# str = ("helo")
+# print(str[0])
+# str[0="y"]
+# detail = ["yama", "age", 45,]
+# print(detail[0])
+# detail[0] = "khan"
+# print(detail) this is used to changes in list
+# slicing= 
+# marks = [34,43,65,73,54]
+# print(marks[0:5 ])
+# str = ("khan snga e khair dy chata")
+# # print(str[6: 8])
+# list = [2,1,5,]
+# list.append(4)
+# print(list) add 4 in it
+# list = [5,8,9,0,3]
+# print(list.sort())
+# # print(list) to arrange these value in ascending order
+# for descending order list. sort(reverse = True)
+# list = ['a', 'b', 'c']
+# list.reverse()
+# print(list)
+# if we want to replace a number that index== 5
+# list = [1,2,3,4,5,6,7,8,]
+# list.insert(1,9)
+# print(list) this mean that we want to enter a value on any index
+# list  = [1,3,4,3,1]
+# list.remove(3)
+# print(list) if we want to remove value from the list4
+# list  = [1,3,4,3,1]
+# list.pop(4)
+# print(list) to delete particular index
+# tup = (1,3,5)
+# print(type(tup))
+# print(tup[0:2])
+#tup[0] = 6 #important comma is must in the tuple without comma they are integeer
+# tup = (1, 6, 7, 8,)
+# print(tup)
+# print(type(tup))
+# print(tup[1:3])      #slicing
+# tup = (1,2,3,4)
+# print(tup.index(1))(1 ) is the vale in the list and the output is rank of it 0
+# tup = (1,2,3,4,4,4,4,4,4,)
+# print(tup.count(4))
